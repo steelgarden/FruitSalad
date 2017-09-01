105128ae7f7ed441287d547c1b2c1ccdf4b22d49
Ben Handley <ben@steelgarden.com>
2017-09-01 20:25:13 +0100

    Multiple fruit working

--- FruitSalad.py
+++ FruitSalad.py
@@ -1,34 +1,29 @@
-class BlueBerry:
-    cost=2
+class Fruit:
     fireDamage=0
-    damage=1
+    damage=0
     coin=True
     immuneFire=False
+    def name(self):
+        return self.__class__.__name__
+    
+class BlueBerry(Fruit):
+    cost=2
+    damage=1
 
-class DragonFruit:
+class DragonFruit(Fruit):
     cost=4
     fireDamage=5
-    damage=0
     coin=False
-    immuneFire=False
 
-class Apple:
+class Apple(Fruit):
     cost=5
-    fireDamage=0
     damage=4
-    coin=True
-    immuneFire=False
 
-class Banana:
+class Banana(Fruit):
     cost=5
-    fireDamage=0
     damage=3
-    coin=True
     immuneFire=True
 
-def fruitName(f):
-    return f.__class__.__name__
-    
 class Player:
     def __init__(self, name):
         self.quarterHearts = 40
@@ -45,37 +40,50 @@ class Player:
         print(self.name+": it's your turn to choose a fruit!")
         
         for id,fruit in enumerate(self.fruits):
-            print(id,fruitName(fruit))
+            print(id,fruit.name())
         print('Which fruit do you want to use? ', end='')
         response=input()
         id=int(response)
         self.currentFruit=self.fruits[id]
-        print('You have chosen to use '+fruitName(self.currentFruit)+'!')
+        print('You have chosen to use '+self.currentFruit.name()+'!')
     
     def action(self):
         for id,player in enumerate(allPlayers):
             print(id,player.name)
-        print(self.name+': which player do you want to target with your '+fruitName(self.currentFruit)+'? ', end='')
+        print(self.name+': which player do you want to target with your '+self.currentFruit.name()+'? ', end='')
         response=input()
         id=int(response)
         target=allPlayers[id]
         print('You have chosen to target '+target.name+'!')
-        target.quarterHearts = target.quarterHearts-1
-        print('Do you want to gain a coin (c) or a quarter heart (h)? ',end='')
-        response=input()
-        if response=='c':
-            self.coins=self.coins+1
-        elif response=='h':
-            self.quarterHearts+=1
+        if target.currentFruit.immuneFire:
+            damageDone=self.currentFruit.damage
         else:
-            print("haha, you can't type so you don't get anything!!!")
+            damageDone=max(self.currentFruit.damage, self.currentFruit.fireDamage)
+        target.quarterHearts -= damageDone
+        if self.currentFruit.coin:
+            print('How many coins do you want to take (max %d)? '%damageDone, end='')
+            response=input()
+            try:
+                coinsTaken=int(response)
+            except:
+                print("I don't get it, no coins for you!")
+                coinsTaken=0
+            if coinsTaken<0 or coinsTaken>damageDone:
+                print("Out of bounds, no coins for you!")
+                coinsTaken=0
+        else:
+            coinsTaken=0
+        self.coins+=coinsTaken
+        self.quarterHearts+=(damageDone-coinsTaken)
         self.stress+=1
         if self.stress==3:
             self.quarterHearts-=1
             self.stress=0
         self.status()
         target.status()
+        
         print()
+        
         
 p1=Player('Ian')
 p2=Player('Ben')
